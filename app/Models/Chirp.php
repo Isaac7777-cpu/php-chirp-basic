17c39326bfa3a3d745f3d76ae9694ea917aba286
Isaac Leong <isaacleong23367@gmail.com>
2025-07-09 17:10:49 +0800

    Finished Sending Out Notification (Untested)

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
+use illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
@@ -10,6 +12,11 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
